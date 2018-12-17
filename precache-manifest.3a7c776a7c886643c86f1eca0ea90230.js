d136ca35fe708d730db8209571375fdd08ec6673
Your Namebatool <batranjbar2018>
2018-12-17 12:26:12 +0330

    Updates

---
+++ precache-manifest.3a7c776a7c886643c86f1eca0ea90230.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90b5287b0e2fb9ccbb51",
+    "url": "/batranjbar2018/resume1/static/css/main.8e82d441.chunk.css"
+  },
+  {
+    "revision": "90b5287b0e2fb9ccbb51",
+    "url": "/batranjbar2018/resume1/static/js/main.90b5287b.chunk.js"
+  },
+  {
+    "revision": "c0b27e49a4d0c49e36a9",
+    "url": "/batranjbar2018/resume1/static/js/1.c0b27e49.chunk.js"
+  },
+  {
+    "revision": "6af2d668cdd9f55c1cbd",
+    "url": "/batranjbar2018/resume1/static/js/runtime~main.6af2d668.js"
+  },
+  {
+    "revision": "c519a29a4ee2af78ef05534f539e0a43",
+    "url": "/batranjbar2018/resume1/index.html"
+  }
+];
